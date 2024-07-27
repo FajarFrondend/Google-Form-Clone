d0d82574f3d54e1b342ada39b28be9f3f6d41334
MUHAMMAD FAJAR KURNIAWAN <133849397+FajarFrondend@users.noreply.github.com>
2024-07-27 07:54:24 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -31,6 +31,76 @@ document.getElementById('myForm').addEventListener('submit', async function(even
 
     console.log('Value of sheet: ', sheet);
 
+    // string massage konfirmasi Pesanan
+    const pesanKonfirmAwal = `Hi ${name}
+                Mohon maaf kami mengganggu waktu anda 🙏
+                Kami dari ${agen}
+                ingin konfirmasi untuk memastikan keberangkatan Travel Sebagai Berikut:
+                Alamat Pick Up: ${alamatJemput}
+                Alamat Tujuan: ${alamatTujuan}
+                Jumlah Pemesanan: ${data} ${tipe}
+                Apa bisa dibantu sharelok penjemputan untuk mengarahkan driver kami pada tanggal Penjemputan ${tanggal} ?🙏
+                Terimakasih Atas Waktunya
+                Semoga Hari ini Penuh Dengan Kebahagiaan🙏`;
+
+    // string massage konfirmasi driver
+    const massageKonfrimDriver = `Hi ${name} 
+                        Mohon maaf kami mengganggu waktu anda 🙏
+
+                        Untuk Driver yang bertugas hari ini adalah :
+                        ${driver1} dan ${driver2} 
+
+                        Dengan Nomer Driver :
+                        ${waDriver} 
+
+                        Terimakasih Atas Pesanan Anda
+                        Selalu Selamat Sampai Tujuan dan Menikmati Perjalanan Bersama Dengan Kami.
+
+                        Salam Hangat dan Sampai bertemu di lain Kesempatan 🙏 `;
+
+    // string massage konfirmasi Final
+    const massageKonfrimFinal = `Hi ${driver1} Dan ${driver2}
+    Selalu Semangat dan Tetap Waspada Dalam Menjalankan Pekerjaan Anda 🙏
+
+    Berikut adalah data Pick Up dan Sharelok Penumpang Anda :
+
+    Nama Penumpang       : ${name}
+    Nomor Pemesan        : ${tiket}
+    Alamat Penjemputan   : ${alamatJemput}
+    Sharelok Penjemputan : ${penjemputan}
+    Tujuan               : ${tujuan}
+    Sharelok Tujuan      : ${tujuan}
+
+    Keterangan PICK UP : ${data} ${agen}
+
+    Tiket  : ${tiket}
+    ST     : ${st}
+    FE     : ${fee}
+
+    AGEN : ${agen}
+
+    Tetap Berdo'a dan Bersyukur Dalam Menjalankan Pekerjaan Anda
+    Kewaspadaan Anda Adalah Harapan Selamat Penumpang Anda
+    Selalu Selamat Sampai Tujuan 😊`;
+
+    // Konforimasi 
+    const encodedPesanKonfirmAwal = encodeURIComponent(pesanKonfirmAwal);
+    const konfirm = `=HYPERLINK("https://api.whatsapp.com/send?phone=${waPesan}&text=${encodedPesanKonfirmAwal}"; "Konfirm")`;
+    // konfirmasi Driver
+    const encodePesanKonfirmDriver = encodeURIComponent(massageKonfrimDriver);
+    const konfirmDriver = `=HYPERLINK("https://api.whatsapp.com/send?phone=${waPesan}&text=${encodePesanKonfirmDriver}"; "RUN")`;
+    // konfirm closing order
+    const encodedPesanKonfirmFinal = encodeURIComponent(massageKonfrimFinal);
+    const konfirmFinal = `=HYPERLINK("https://api.whatsapp.com/send?phone=${waPesan}&text=${encodedPesanKonfirmFinal}"; "CLOSING")`
+
+    // Masukkan nilai tersebut ke textarea
+    document.getElementById('massage-konfrim-final').value = massageKonfrimFinal;
+    document.getElementById('massage').value = pesanKonfirmAwal;
+    document.getElementById('massage-konfrim-driver').value = massageKonfrimDriver;
+    document.getElementById('konfirm').value = konfirm;
+    document.getElementById('konfirmDriver').value = konfirmDriver;
+    document.getElementById('konfirmFinal').value = konfirmFinal;
+
     // Buat objek data dengan nilai dari form
     const formData = {
         sheet,
@@ -44,18 +114,24 @@ document.getElementById('myForm').addEventListener('submit', async function(even
         tiket,
         st,
         fee,
+        konfirm,
+        konfirmDriver,
+        konfirmFinal,
+        pesanKonfirmAwal,
         waPesan,
         driver1,
         driver2,
         waDriver,
         penjemputan,
-        tujuan
+        tujuan,
+        massageKonfrimDriver,
+        massageKonfrimFinal 
     };
 
     console.log('Data to be sent:', formData); 
 
     try {
-        const response = await fetch('https://new-express-project-j1jyyure4-benjis-projects-2d9aa7eb.vercel.app/submit', {
+        const response = await fetch('https://new-express-project-j1jyyure4-benjis-projects-2d9aa7eb.vercel.app/submit', { // URL backend Express.js https://backend-fcnj4p8iv-benjis-projects-2d9aa7eb.vercel.app/submit
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(formData)
